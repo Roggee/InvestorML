9f8dea810b3e4cc1c621da9f81fbb0cab06b2cad
Percy Villajulca <percyvillajulcar@gmail.com>
2024-06-13 09:46:36 -0500

    1. Actualziación de valores estáticos 2. Ajustar función para calcular postura inicial. 3. Implementar funcionar nueva: evaluarCambioCamino,  avanzarCaminata, IniciarCaminata, EvaluarDestino, EvaluarFinCaminoLaboral, activarFicha y reposarFicha

--- models/jugador.js
+++ models/jugador.js
@@ -1,5 +1,7 @@
 const Casillas = require('./casillas');
+const Ruta = require('./ruta');
 const THREE = require('three');
+const {PE,CA_POS_INTERNAS} = require("./valores");
 
 class Jugador{
   static FICHA_ESTADO_ESPERAR = 1;
@@ -56,34 +58,187 @@ class Jugador{
   /**
    * Calcular posición y postura según si es jugador actual o no
    */
-  calcularTransformacionInicial(){
-    let p = new THREE.Matrix4();
-    let transformacion = new THREE.Matrix4();
+  setPosicionInicial(){
     if(this.id == this.partida.jugadorActual.id){
-      p.makeRotationY(-Math.PI/2);
+      this.activarFicha();
     }else{
-      let vpi = Casillas.POS_INTERNAS[this.posRelativa];
-      transformacion.makeTranslation(vpi[0],vpi[1],vpi[2]);
-      //console.log(`jugador `+this.nombre+` posInterna [`+vpi+'] por '+this.posRelativa);
+      this.reposarFicha();
     }
-    transformacion.multiply(p);
+  }
+
+  evaluarCambioCamino(idcasilla) {
+    const casDef = this.partida.tablero.casillerosDef.items;
+    const rutaEsp = casDef[this.posicion].rutaEspecial;
+    let ruta;
+    if(rutaEsp && rutaEsp.ruta[rutaEsp.ruta.length-1]==idcasilla){
+        ruta = new Ruta(rutaEsp.ruta,0);
+    }else{
+        ruta = new Ruta([this.posicion,idcasilla],0);
+    }
+    console.log(JSON.stringify(ruta));
+    // TODO: revisar necesidad de clase Variables
+    // $vars = new Variables();
+    // $vars->guardar($idpartida,"ruta",json_encode($ruta), $cnn);
+    this.partida.estadoInicial = this.partida.estado;
+    this.fichaEstado = Jugador.FICHA_ESTADO_ESPERAR;
+    this.partida.tablero.limpiar();
+    this.partida.estado = PE.CAMINANDO;
+
+    return ruta;
+  }
+  avanzarCaminata(ruta){
+    const numSegmentos = 20;//25;
+    const casDef = this.partida.tablero.casillerosDef.items;
+    const Vangle = new THREE.Vector3();
+    const P = new THREE.Vector3();
+    const temp = new THREE.Matrix4();
+    const transformacion = new THREE.Matrix4();
+    let [ini,fin,iSegmento,iCasillaActual] = [0,0,0,0];
+    //console.log(`avanzarCaminata: ${JSON.stringify(ruta)}`);
+    this.fichaEstado = Jugador.FICHA_ESTADO_CAMINAR;
+    const intervalID = setInterval(()=> {
+        if(iCasillaActual<ruta.getLongitud()){
+            transformacion.identity();
+            iSegmento++;
+            if(iSegmento<numSegmentos){
+                if(iSegmento==1){
+                    ini = ruta.get(iCasillaActual);
+                    fin = ruta.get(iCasillaActual+1);
+                    Vangle.subVectors(casDef[fin].coords,casDef[ini].coords);
+                }
+                P.lerpVectors(casDef[ini].coords,casDef[fin].coords,iSegmento/numSegmentos);
+            }else{
+                iSegmento = 0;                            
+                iCasillaActual++;
+                const v = casDef[ruta.get(iCasillaActual)].coords;
+                P.copy(v);
+            }
+            const giro = Math.atan2(-Vangle.z,Vangle.x);
+            temp.makeRotationY(giro);
+            transformacion.multiply(temp);
+            temp.makeTranslation(P);
+            transformacion.premultiply(temp);
+            const idCasillaActual = ruta.get(iCasillaActual);
+            this.posicion = idCasillaActual;
+            this.fichaTransform = transformacion.toArray();
+            this.transmitir();
+            //console.log("caminando...");
+        }else{
+            clearInterval(intervalID);
+            this.terminarCaminata(ruta,false);
+        }
+    },100);
+  }
+  /**
+   * Actualiza el estado de la ficha y limpia el tablero
+   */
+  iniciarCaminata(){
+    this.fichaEstado = Jugador.FICHA_ESTADO_CAMINAR;
+    this.partida.tablero.limpiar();
+  }
+  /**
+   * @param ruta la ruta que caminó
+   * @param forzado flag para indicar si la animación de caminanta fue terminada anticipadamente
+   */
+  terminarCaminata(ruta,forzado){
+    //iSegmento=0;
+    //iCasillaActual = 0;
+    //setEnable(false);
+    //ServicioPartida.SP().terminarCaminata(forzado);
+    this.fichaEstado = Jugador.FICHA_ESTADO_ESPERAR;
+    this.transmitir();
+    //se espera antes de proceder con la evaluación de la casilla para apreciar donde cayó
+    setTimeout(() => {
+        this.evaluarDestino(ruta);
+        this.partida.transmitir();
+    }, 500);
+  }
+  evaluarDestino(ruta) {
+    this.partida.estado = PE.EVALUANDO_DESTINO;
+    
+    if(ruta.getLongitud()!=7){ //NO es feriado
+        this.evaluarFinCaminoLaboral(ruta);
+    }else{ //es un feriado
+        // $dialogo = new Dialogo();
+        // $dialogo->abrir($idpartida, Dialogo::AVISO_FERIADO, "Es feriado. Puedes descansar...zzZ", $cnn);
+        console.log("pendiente implementar Feriado");
+        this.evaluarFinCaminoLaboral(ruta);
+    }
+  }
+  evaluarFinCaminoLaboral(ruta) {
+    //$this->cobrarSueldo($idpartida,$jugador,$ruta->numMeses,$cnn); //cobrar sueldo por los meses pasados
+    //if($partida->evaluarGanador($idpartida, $cnn)) return;
+    this.partida.tablero.procesarCasilla(this,ruta); //procesa CASILLA ACTUAL
+  }
+  /**
+   * Devuelve FALSE si el siguiente jugador debe descansar en los demas casos de vuelve TRUE.
+   */
+  terminarTurno() {    
+    //Limpiar acción de FUSIÓN no finalizada
+    //$variable->tomar($idpartida, "fusionT0", $cnn);
+    this.partida.tablero.limpiar();
+
+    //calcular siguiente jugador y asignar animación de ficha correspondiente
+    let jActual = this.partida.getJugadorSiguiente(this.orden);
+    this.partida.jugadorActual = jActual;
+    this.reposarFicha();
+    jActual.activarFicha();
+    
+    //validar jugadores con turnos de descanso
+    if(jActual.turnosDescanso>=1){
+      console.log("pendiente implementar cuando hay turnos descansando");
+        // $jActual->reducirDescanso($jActualId,$cnn);            
+        // $msj = "@j$jActual->id descansará 1 turno mas...";
+        // $dialogo = new Dialogo();
+        // $dialogo->abrir($idpartida, Dialogo::AVISO_DESCANSO, $msj, $cnn);
+        return false;
+    }else{
+      this.partida.tablero.limpiar();
+      this.partida.tablero.permitirCambiarCarril(jActual.posicion);
+      this.partida.d1Ix=-1;
+      this.partida.d2Ix=-1;
+      this.partida.dVal=-1;
+      //this.partida.btnAccion=Partida.BOTON_ACCION_LANZAR;
+      return true;
+    }
+  }
+  activarFicha(){
+    let transformacion = new THREE.Matrix4();
+    transformacion.makeRotationY(-Math.PI/2);
     const csl = this.partida.tablero.casillerosDef.items[this.posicion];
-    //console.log(`jugador `+this.nombre+` Coords: `+JSON.stringify(csl)+' por '+this.posicion);
+    let p = new THREE.Matrix4();
     p.makeTranslation(csl.coords);
     transformacion.premultiply(p);
     this.fichaTransform = transformacion.toArray();
-    //console.log(`jugador `+this.nombre+` fichaTransform: ${this.fichaTransform}`);
+    this.fichaEstado = Jugador.FICHA_ESTADO_SALUDO;
+    this.posRelativa = -1;
+    this.partida.tablero.updatePosInternasCasilla();
   }
-    /**
-     * Envia estado PARCIAL del juego con los datos de un jugador a todos los jugadores de la partida actual.
-     */
-    transmitir(){
-      this.partida.jugadores.forEach( j => {
-          let rsp = {type:"game",content:{jugador:this.minify(),msj:""}};
-          j.wsclient.send(JSON.stringify(rsp));
-      });
-    }  
-
+  reposarFicha(){
+    const csl = this.partida.tablero.casillerosDef.items[this.posicion];
+    const iLibre = this.partida.tablero.getPosicionLibre(this.posicion);
+    let transformacion = new THREE.Matrix4();
+    transformacion.makeRotationY(csl.giro*Math.PI/180.0);
+    let vpi = CA_POS_INTERNAS[iLibre];
+    let p = new THREE.Matrix4();
+    p.makeTranslation(vpi[0],vpi[1],vpi[2]);
+    transformacion.premultiply(p);
+    p.makeTranslation(csl.coords);
+    transformacion.premultiply(p);
+    this.fichaTransform = transformacion.toArray();
+    this.fichaEstado = Jugador.FICHA_ESTADO_ESPERAR;
+    this.posRelativa = iLibre;
+    this.partida.tablero.updatePosInternasCasilla();
+  }  
+  /**
+   * Envia estado PARCIAL del juego con los datos de un jugador a todos los jugadores de la partida actual.
+   */
+  transmitir(){
+    this.partida.jugadores.forEach( j => {
+        let rsp = {type:"game",content:{jugador:this.minify(),msj:""}};
+        j.wsclient.send(JSON.stringify(rsp));
+    });
+  }
 }
 
 module.exports = Jugador;
